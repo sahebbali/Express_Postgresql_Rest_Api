4fec1940b85aec3adbc0134ba94738af10d1252b
sahebbali <saheb1996jmr@gmail.com>
2025-02-04 21:43:07 +0600

    add employee table

--- src/data/data.sql
+++ src/data/data.sql
@@ -4,3 +4,19 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 )
+
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(15),
+    hire_date DATE NOT NULL,
+    job_title VARCHAR(100) NOT NULL,
+    department VARCHAR(100),
+    salary NUMERIC(10, 2) NOT NULL,
+    manager_id INT,
+    CONSTRAINT fk_manager
+        FOREIGN KEY (manager_id) 
+        REFERENCES employees(employee_id)
+);
