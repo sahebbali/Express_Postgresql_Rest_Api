673e03b30a0bfc54cb7b01354ad9b517dc65c77c
sahebbali <saheb1996jmr@gmail.com>
2025-02-10 17:06:14 +0600

    add department table

--- src/data/departmentTable.js
+++ src/data/departmentTable.js
@@ -1,27 +1,8 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-)
+import pool from "../config/db.js";
 
-CREATE TABLE IF NOT EXISTS employees (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone_number VARCHAR(15),
-    hire_date DATE NOT NULL,
-    job_title VARCHAR(100) NOT NULL,
-    department VARCHAR(100),
-    salary NUMERIC(10, 2) NOT NULL,
-    manager_id INT,
-    CONSTRAINT fk_manager
-        FOREIGN KEY (manager_id) 
-        REFERENCES employees(employee_id)
-);
-
-CREATE TABLE IF NOT EXISTS department (
+const createEmployeeTable = async () => {
+  const queryText = `
+   CREATE TABLE IF NOT EXISTS department (
     id SERIAL PRIMARY KEY, -- Auto-incrementing primary key
     name VARCHAR(255) NOT NULL, -- Name of the department
     description TEXT, -- Description of the department (optional)
@@ -34,3 +15,14 @@ CREATE TABLE IF NOT EXISTS department (
         REFERENCES user(id) -- Assuming you have a 'user' table for employees
         ON DELETE SET NULL -- Set manager_id to NULL if the referenced user is deleted
 );
+    `;
+
+  try {
+    pool.query(queryText);
+    console.log("Employee table created if not exists");
+  } catch (error) {
+    console.log("Error creating users table : ", error);
+  }
+};
+
+export default createEmployeeTable;
